c23edb422179eef0b8329d7ba0425312f5e7cd91
Toni Thomas <tsthomas205@gmail.com>
2025-09-05 03:16:33 -0400

    Fix vite.config.js with base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/react-portfolio/", // 👈 this matches your repo name
   plugins: [react()],
-  base: mode === "production" ? "/react-portfolio/" : "/",
-}));
+});
